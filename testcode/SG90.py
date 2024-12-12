62a8b8bb63f2482daa8b060a39a4c8aed29158e2
kasuta <poguxiang@gmail.com>
2024-12-12 19:53:10 +0900

    SG90 調整1

--- testcode/SG90.py
+++ testcode/SG90.py
@@ -50,38 +50,13 @@ class SG90:
         self.pi.set_mode(self.pin, pigpio.INPUT)
         self.pi.stop()
 
-    # def set_angle(self, target_angle: int) -> None:
-    #     """
-    #     サーボモータを指定角度に移動する
-
-    #     Args:
-    #         target_angle (int): 動作させる角度
-    #     """
-    #     if target_angle < self.min_angle or target_angle > self.max_angle:
-    #         print(f"角度は{self.min_angle}から{self.max_angle}度の間で指定してください。")
-    #         return
-
-    #     # 角度を0.5から2500のパルス幅にマッピングする(SG90の仕様に合わせたつもりだが,pulse_widthの仕様が不明なためちょっと怪しい.引数の単位が謎なんよな．今はμs pulse)
-    #     pulse_width = ((target_angle+90) / 180) * (2400 - 500) + 500
-    #     self.angle = target_angle
-    #     # パルス幅を設定してサーボを回転させる
-        # self.pi.set_servo_pulsewidth(self.pin, pulse_width)
-    #     time.sleep(0.2)  # サーボモータが移動するのを待つために少し待つ
-
-    # def set_ini_angle(self) -> None:
-    #     """
-    #     サーボモータを初期設定の角度に移動する
-    #     """
-    #     self.set_angle(self.ini_angle)
-
-
     #set_servo_pulsewidthを使わない方法(案)
     def set_angle(self,target_angle: int) ->None:
         if target_angle < self.min_angle or target_angle > self.max_angle:
             print(f"角度は{self.min_angle}から{self.max_angle}度の間で指定してください。")
             return
         #角度[degree]→パルス幅[μs]に変換
-        pulse_width = ((target_angle+90) / 180) * (2250 - 750) + 750
+        pulse_width = ((target_angle+self.max_angle) / self.min_angle+self.max_angle) * (2250 - 750) + 750
         self.angle = target_angle
         #duty比[%]を計算(周期：20ms=20000μs)
         pwm_duty = 100*(pulse_width/20000)
@@ -113,43 +88,21 @@ if __name__ == "__main__":
     """
     
     #これサンプルコードだから0~180のangle採用してる。書き換える必要あり。
-    # servo_motor_horizontal = ServoMotor(pin=17, max_angle=160, min_angle=0, ini_angle=90)
-    # servo_motor_vertical = ServoMotor(pin=18, max_angle=130, min_angle=0, ini_angle=40)
-    sg90 = SG90( pin=17, min_angle=-90, max_angle=90, ini_angle=0,freq=50)
+    sg90 = SG90( pin=17, min_angle=-70, max_angle=70, ini_angle=0,freq=50)
 
     # サーボ動作開始
-    # servo_motor_horizontal.start()
-    # servo_motor_vertical.start()
     sg90.start()
 
 
     # サンプル動作 
-    # servo_motor_vertical.set_angle(20)
-    # for angle in range(0, 181, 10):
-    #     servo_motor_horizontal.set_angle(angle)
-
-    # servo_motor_vertical.set_angle(40)
-    # for angle in range(180, -1, -10):
-    #     servo_motor_horizontal.set_angle(angle)
-
-    # servo_motor_vertical.set_angle(60)
-    # for angle in range(0, 181, 10):
-    #     servo_motor_horizontal.set_angle(angle)
-    sg90.set_angle(10)
     print(sg90.angle)
 
     # 初期位置(ini_angle)に移動
-    # servo_motor_vertical.set_ini_angle()
-    # servo_motor_horizontal.set_ini_angle()
     sg90.set_ini_angle()
     time.sleep(3)
     # 現在の角度を表示
-    # print(servo_motor_vertical.angle)
-    # print(servo_motor_horizontal.angle)
     print(sg90.angle)
     time.sleep(3)
 
     # サーボ動作停止
-    # servo_motor_horizontal.stop()
-    # servo_motor_vertical.stop()
     sg90.stop()
