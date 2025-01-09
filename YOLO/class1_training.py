4d3f1d980e9502afd5c83ffd8070e2070f87872c
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-01-10 00:39:03 +0900

    ディレクトリ構造変更，相対パス化
    これですぐ動く
    datasetsとdatasetが2連続なのは内緒

--- YOLO/class1_training.py
+++ YOLO/class1_training.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov10n.pt")
 
 # モデルをyamlファイルでトレーニング
-model.train(data="C:/Users/hiyos/OneDrive - 埼玉大学/あかとき/プログラム/SC-25/yolo_class1/dataset/training.yaml", epochs=100, imgsz=640)
+model.train(data="./datasets/dataset/training.yaml", epochs=100, imgsz=640)
 
 # モデルをエクスポート
 # model.save("yolov10n_test.pt")
