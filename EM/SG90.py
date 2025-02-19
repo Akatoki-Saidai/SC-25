1c84ff2a981b2f2a9cafb3f5ae7bd7d0d6afe03a
n17y17 <N17Y17@outlook.com>
2025-02-19 17:32:55 +0900

    セッタがあるならゲッタもあるよね

--- EM/SG90.py
+++ EM/SG90.py
@@ -18,41 +18,44 @@ class SG90:
         """
         サーボモータを制御するクラス
         """
-        self.pin = pin
-        self.max_angle = max_angle
-        self.min_angle = min_angle
-        self.angle = 0
-        self.ini_angle = ini_angle
-        self.freq = freq
-        self.pwm = PWM(Pin(self.pin))
-        self.pwm.freq(self.freq)
+        self._pin = pin
+        self._max_angle = max_angle
+        self._min_angle = min_angle
+        self._angle = 0
+        self._ini_angle = ini_angle
+        self._freq = freq
+        self._pwm = PWM(Pin(self._pin))
+        self._pwm.freq(self._freq)
 
     def set_angle(self, target_angle: int) -> None:
         """
         サーボモータを指定した角度に設定する
         """
-        if target_angle < self.min_angle or target_angle > self.max_angle:
-            logger.error(f"角度は{self.min_angle}から{self.max_angle}度の間で指定してください。")
+        if target_angle < self._min_angle or target_angle > self._max_angle:
+            logger.error(f"角度は{self._min_angle}から{self._max_angle}度の間で指定してください。")
             return
         # 角度[degree] → パルス幅[μs]に変換
-        pulse_width = ((target_angle + self.max_angle) / (self.max_angle - self.min_angle)) * (2000 - 1000) + 1000
-        self.angle = target_angle
+        pulse_width = ((target_angle + self._max_angle) / (self._max_angle - self._min_angle)) * (2000 - 1000) + 1000
+        self._angle = target_angle
         # duty比（0-1023の範囲）に変換
         duty_cycle = int((pulse_width / 20000) * 1023)
         # PWM出力
-        self.pwm.duty_u16(duty_cycle)
+        self._pwm.duty_u16(duty_cycle)
+    
+    def get_angle(self):
+        return self._angle
 
     def set_ini_angle(self) -> None:
         """
         初期設定角度（ini_angle）に設定
         """
-        self.set_angle(self.ini_angle)
+        self.set_angle(self._ini_angle)
 
     def stop(self) -> None:
         """
         PWM信号を停止
         """
-        self.pwm.duty_u16(0)
+        self._pwm.duty_u16(0)
 
 if __name__ == "__main__":
     # 動作サンプル
@@ -61,14 +64,14 @@ if __name__ == "__main__":
     sg90 = SG90(pin=18, min_angle=-90, max_angle=90, ini_angle=0, freq=50)
 
     # サーボ動作開始
-    print(f"現在の角度: {sg90.angle}")
+    print(f"現在の角度: {sg90.get_angle()}")
 
     sg90.set_angle(10)
     # 初期位置(ini_angle)に移動
     sg90.set_ini_angle()
     time.sleep(3)
     # 現在の角度を表示
-    print(f"現在の角度: {sg90.angle}")
+    print(f"現在の角度: {sg90.get_angle()}")
     time.sleep(3)
 
     # サーボ動作停止
