e0adf9ccdd035caab96f84dc307fea243e687965
n17y17 <N17Y17@outlook.com>
2025-02-22 01:05:00 +0900

    GNSSを修正

--- EM/gnss.py
+++ EM/gnss.py
@@ -15,38 +15,22 @@ class GNSS:
             # for sentence in sentence_list[-11:-2]:
             for x in sentence:
                 if 10 <= ord(x) <= 126:
-                    print(x)
-                    stat = self._pygps.update(x)
-                    if stat:
-                        print(self._pygps.latitude[0],self._pygps.longitude[0] )
+                    print(x, end="")
+                    self._pygps.update(x)
 
     def __init__(self):
         self._uart = serial.Serial('/dev/serial0', 38400, timeout = 10)
         self._pygps = MicropyGPS(9, 'dd')
         self._read_thread = Thread(target=self._update)
         self._read_thread.start()
-        self._read_thread.join()
     
-    # def get_forever(self, data):
-    #     while True:
-    #         try:
-    #             sentence_all = uart.read(uart.in_waiting).decode('utf-8')
-    #             sentence_list = sentence_all.split('\n')
-    #             for sentence in sentence_list[-11:-2]:
-    #                 for x in sentence:
-    #                     if 10 <= x <= 126:
-    #                         #print("5")
-    #                         stat = my_gps.update(chr(x))
-    #                         #print("stat:",stat,"x:",x,"chr:",chr(x))
-    #                         #print(chr(x))
-    #                         if stat:
-    #                             #print("6")
-    #                             tm = my_gps.timestamp
-    #                             tm_now = (tm[0] * 3600) + (tm[1] * 60) + int(tm[2])
-    #                             if (tm_now - tm_last) >= 10:
-    #                                 print('=' * 20)
-    #                                 print(my_gps.date_string(), tm[0], tm[1], int(tm[2]))
-    #                                 print("latitude:", my_gps.latitude[0], ", longitude:", my_gps.longitude[0])
+    def get(self):
+        while True:
+            try:
+                if 0 < self._pygps.parsed_sentences:
+                    logger.info(f"lat: {self._pygps.latitude[0]}, lon: {self._pygps.longitude[0]}")
+            except Exception as e:
+                logger.exception("An error occured!")
 
 
 def main():
@@ -84,4 +68,5 @@ def main():
 
 if __name__ == "__main__":
     gnss = GNSS()
+    gnss.get()
     # main()
