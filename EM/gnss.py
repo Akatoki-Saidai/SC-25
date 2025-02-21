9fb424afa3f3368425a27ebee0794a68a885bdc5
n17y17 <N17Y17@outlook.com>
2025-02-22 01:37:00 +0900

    gnssを簡潔に修正

--- EM/gnss.py
+++ EM/gnss.py
@@ -24,49 +24,25 @@ class GNSS:
         self._read_thread = Thread(target=self._update)
         self._read_thread.start()
     
-    def get(self):
+    def get_forever(self, data):
         while True:
             try:
                 if 0 < self._pygps.parsed_sentences:
-                    logger.info(f"lat: {self._pygps.latitude[0]}, lon: {self._pygps.longitude[0]}, alt: {self._pygps.altitude}, speed: {self._pygps.speed}, date: {self._pygps.date}, time: {self._pygps.timestamp}")
+                    lat = self._pygps.latitude[0]
+                    lon = self._pygps.longitude[0]
+                    logger.debug(f"lat: {lat}, lon: {lon}, alt: {self._pygps.altitude}, speed: {self._pygps.speed}, date: {self._pygps.date}, time: {self._pygps.timestamp}")
+                    if lat and lon:
+                        data["lat"] = lat
+                        data["lon"] = lon
             except Exception as e:
                 logger.exception("An error occured!")
 
 
 def main():
-    #print("1")
-    # シリアル通信設定
-    uart = serial.Serial('/dev/serial0', 38400, timeout = 10)
-    # gps設定
-    my_gps = MicropyGPS(9, 'dd')
-    #print("2")
-    # 10秒ごとに表示
-    tm_last = 0
-    while True:
-        #print("3")
-        sentence_all = uart.read(uart.in_waiting).decode('utf-8')
-        print("GPS data received")
-        sentence_list = sentence_all.split('\n')
-        #print(len(sentence))
-        #continue
-        for sentence in sentence_list[-11:-2]:
-            #print("4")
-            for x in sentence:
-                if 10 <= x <= 126:
-                    #print("5")
-                    stat = my_gps.update(chr(x))
-                    #print("stat:",stat,"x:",x,"chr:",chr(x))
-                    #print(chr(x))
-                    if stat:
-                        #print("6")
-                        tm = my_gps.timestamp
-                        tm_now = (tm[0] * 3600) + (tm[1] * 60) + int(tm[2])
-                        if (tm_now - tm_last) >= 10:
-                            print('=' * 20)
-                            print(my_gps.date_string(), tm[0], tm[1], int(tm[2]))
-                            print("latitude:", my_gps.latitude[0], ", longitude:", my_gps.longitude[0])
+    gnss = GNSS()
+    data = {"lat": None, "lon": None}
+    gnss.get_forever(data)
+
 
 if __name__ == "__main__":
-    gnss = GNSS()
-    gnss.get()
-    # main()
+    main()
