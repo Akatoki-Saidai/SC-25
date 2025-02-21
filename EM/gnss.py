edb360fd42ea8b3ab4850fb4e678ab67d019ea28
n17y17 <N17Y17@outlook.com>
2025-02-22 00:07:54 +0900

    GNSSをスレッドで動かすテストコード

--- EM/gnss.py
+++ EM/gnss.py
@@ -2,8 +2,48 @@ import time
 import serial
 from micropyGPS import MicropyGPS
 import sc_logging
+from threading import Thread
 
 logger = sc_logging.get_logger(__name__)
+
+class GNSS:
+    def _update(self):
+        sentence_all = self._uart.read(self._uart.in_waiting).decode('utf-8')
+        print(sentence_all)
+        sentence_list = sentence_all.split('\n')
+        # for sentence in sentence_list[-11:-2]:
+        #     for x in sentence:
+        #         if 10 <= x <= 126:
+        #             #print("5")
+        #             stat = my_gps.update(chr(x))
+
+    def __init__(self):
+        self._uart = serial.Serial('/dev/serial0', 38400, timeout = 10)
+        self._pygps = MicropyGPS(9, 'dd')
+        self._read_thread = Thread(target=self._update)
+        self._read_thread.start()
+    
+    # def get_forever(self, data):
+    #     while True:
+    #         try:
+    #             sentence_all = uart.read(uart.in_waiting).decode('utf-8')
+    #             sentence_list = sentence_all.split('\n')
+    #             for sentence in sentence_list[-11:-2]:
+    #                 for x in sentence:
+    #                     if 10 <= x <= 126:
+    #                         #print("5")
+    #                         stat = my_gps.update(chr(x))
+    #                         #print("stat:",stat,"x:",x,"chr:",chr(x))
+    #                         #print(chr(x))
+    #                         if stat:
+    #                             #print("6")
+    #                             tm = my_gps.timestamp
+    #                             tm_now = (tm[0] * 3600) + (tm[1] * 60) + int(tm[2])
+    #                             if (tm_now - tm_last) >= 10:
+    #                                 print('=' * 20)
+    #                                 print(my_gps.date_string(), tm[0], tm[1], int(tm[2]))
+    #                                 print("latitude:", my_gps.latitude[0], ", longitude:", my_gps.longitude[0])
+
 
 def main():
     #print("1")
@@ -39,4 +79,5 @@ def main():
                             print("latitude:", my_gps.latitude[0], ", longitude:", my_gps.longitude[0])
 
 if __name__ == "__main__":
-    main()
+    gnss = GNSS()
+    # main()
