1d31596f5cea323907e38fd2740e95a0ff4cb913
n17y17 <N17Y17@outlook.com>
2025-02-22 00:27:14 +0900

    test

--- EM/gnss.py
+++ EM/gnss.py
@@ -9,14 +9,15 @@ logger = sc_logging.get_logger(__name__)
 class GNSS:
     def _update(self):
         while True:
-            sentence_all = self._uart.read(self._uart.in_waiting).decode('utf-8')
-            print(sentence_all)
-            sentence_list = sentence_all.split('\n')
+            sentence = self._uart.read(self._uart.in_waiting).decode('utf-8')
+            # print(sentence_all)
+            # sentence_list = sentence_all.split('\n')
             # for sentence in sentence_list[-11:-2]:
-            #     for x in sentence:
-            #         if 10 <= x <= 126:
-            #             #print("5")
-            #             stat = my_gps.update(chr(x))
+            for x in sentence:
+                if 10 <= x <= 126:
+                    #print("5")
+                    stat = self._pygps.update(chr(x))
+            print(self._pygps.latitude[0],self._pygps.longitude[0] )
 
     def __init__(self):
         self._uart = serial.Serial('/dev/serial0', 38400, timeout = 10)
