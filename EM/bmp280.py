b28012f9e5c420801bb892ad08a7eb4740c764e6
n17y17 <N17Y17@outlook.com>
2025-02-25 20:38:07 +0900

    変数名を変更

--- EM/bmp280.py
+++ EM/bmp280.py
@@ -20,7 +20,7 @@ class BMP280:
             # 測定値の生データを受信
             data = []
             for i in range (0xF7, 0xF7+8):
-                data.append(self.bus.read_byte_data(self.i2c_address,i))
+                data.append(self.bus.read_byte_data(self.I2C_ADDRESS,i))
             
             pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
             temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
@@ -49,9 +49,9 @@ class BMP280:
             logger.setLevel(10)
         self.logger = logger
 
-        self.bus_number  = 1  # I2C1を使用(デフォルト)
-        self.i2c_address = 0x76  # BMP280のI2Cアドレス．通信先のセンサの電話番号のようなもの．0x76(デフォルト)または0x77
-        self.bus = SMBus(self.bus_number)  # I2Cを扱うsmbusを定義
+        self.BUS_NUMBER  = 1  # I2C1を使用(デフォルト)
+        self.I2C_ADDRESS = 0x76  # BMP280のI2Cアドレス．通信先のセンサの電話番号のようなもの．0x76(デフォルト)または0x77
+        self.bus = SMBus(self.BUS_NUMBER)  # I2Cを扱うsmbusを定義
 
         self._setup()  # 測定方法や補正方法を設定
         self._get_calib_param()  # 補正用パラメータの読み取りと保存
@@ -67,11 +67,11 @@ class BMP280:
     def _setup(self):
         try:
             osrs_t = 1  # 気温のオーバーサンプリング (複数回測定すると精度が上がり測定にかかる時間が伸びる．0:測定しない, 1:1回, 2:2回, 3:4回, 4:8回, 5:16回)
-            osrs_p = 5  # 気圧のオーバーサンプリング (複数回測定すると精度が上がり測定にかかる時間が伸びる．0:測定しない, 1:1回, 2:2回, 3:4回, 4:8回, 5:16回)
+            osrs_p = 2  # 気圧のオーバーサンプリング (複数回測定すると精度が上がり測定にかかる時間が伸びる．0:測定しない, 1:1回, 2:2回, 3:4回, 4:8回, 5:16回)
             osrs_h = 1  # 湿度のオーバーサンプリング (複数回測定すると精度が上がり測定にかかる時間が伸びる．0:測定しない, 1:1回, 2:2回, 3:4回, 4:8回, 5:16回) (BME280では使うがBMP280では不使用)
             mode   = 3  # 電力モード (0:sleep, 1:forced(1回だけすばやく測定), 3:normal(t_sb間隔で何度も測定))
-            t_sb   = 4  # normalモードにおける測定(データの更新)間隔  (0:0.5ms, 1:62.5ms, 2:125ms, 3:250ms, 4:500ms, 5:1000ms, 6:2000ms, 7:4000ms)
-            filter = 0  # ノイズを除去するIIRフィルタ (0:off, 1,2,3,4:on(数字が増えると精度が上がり消費電力も上がる))
+            t_sb   = 1  # normalモードにおける測定(データの更新)間隔  (0:0.5ms, 1:62.5ms, 2:125ms, 3:250ms, 4:500ms, 5:1000ms, 6:2000ms, 7:4000ms)
+            filter = 1  # ノイズを除去するIIRフィルタ (0:off, 1,2,3,4:on(数字が増えると精度が上がり消費電力も上がる))
             spi3w_en = 0  # 3線式SPIを有効にするか (I2Cを使うので無効化しておく)
 
             # 設定データを送信用のバイト列に加工
@@ -89,7 +89,7 @@ class BMP280:
     # I2CにてBME280にデータを送信
     def _writeReg(self, reg_address, data):
         try:
-            self.bus.write_byte_data(self.i2c_address,reg_address,data)  # smbusを使用しI2Cでデータを送信 (i2cアドレスは誰宛のデータかを示す，レジスタアドレスはセンサのメモリ内の番地を表す)
+            self.bus.write_byte_data(self.I2C_ADDRESS,reg_address,data)  # smbusを使用しI2Cでデータを送信 (i2cアドレスは誰宛のデータかを示す，レジスタアドレスはセンサのメモリ内の番地を表す)
         except Exception as e:
             self.logger.exception("An error occured!")
 
@@ -97,53 +97,53 @@ class BMP280:
     def _get_calib_param(self):
         try:
             # 補正用パラメータを初期化
-            self.digT = [0, 0, 0]  # 温度の補正パラメータ
-            self.digP = [0, 0, 0, 0, 0, 0, 0, 0, 0]  # 気圧の補正パラメータ
-            self.digH = [0, 0, 0, 0, 0, 0]  # 湿度の補正パラメータ(BME280の名残で書いてあるだけで，BMP280では不使用)
-            self.t_fine = 0.0  # 気圧の補正に使う気温の値
+            self._digT = [0, 0, 0]  # 温度の補正パラメータ
+            self._digP = [0, 0, 0, 0, 0, 0, 0, 0, 0]  # 気圧の補正パラメータ
+            self._digH = [0, 0, 0, 0, 0, 0]  # 湿度の補正パラメータ(BME280の名残で書いてあるだけで，BMP280では不使用)
+            self._t_fine = 0.0  # 気圧の補正に使う気温の値
 
             calib = []  # 受信した補正用生データをここに格納
         
             # 補正用パラメータを受信
             for i in range (0x88,0x88+24):
-                calib.append(self.bus.read_byte_data(self.i2c_address,i))
-            calib.append(self.bus.read_byte_data(self.i2c_address,0xA1))
+                calib.append(self.bus.read_byte_data(self.I2C_ADDRESS,i))
+            calib.append(self.bus.read_byte_data(self.I2C_ADDRESS,0xA1))
             for i in range (0xE1,0xE1+7):
-                calib.append(self.bus.read_byte_data(self.i2c_address,i))
+                calib.append(self.bus.read_byte_data(self.I2C_ADDRESS,i))
 
             # 受信した補正用生データを加工して保存
-            self.digT[0] = ((calib[1] << 8) | calib[0])  # 気温補正データ
-            self.digT[1] = ((calib[3] << 8) | calib[2])
-            self.digT[2] = ((calib[5] << 8) | calib[4])
-            self.digP[0] = ((calib[7] << 8) | calib[6])  # 気圧補正データ
-            self.digP[1] = ((calib[9] << 8) | calib[8])
-            self.digP[2] = ((calib[11]<< 8) | calib[10])
-            self.digP[3] = ((calib[13]<< 8) | calib[12])
-            self.digP[4] = ((calib[15]<< 8) | calib[14])
-            self.digP[5] = ((calib[17]<< 8) | calib[16])
-            self.digP[6] = ((calib[19]<< 8) | calib[18])
-            self.digP[7] = ((calib[21]<< 8) | calib[20])
-            self.digP[8] = ((calib[23]<< 8) | calib[22])
-            self.digH[0] = ( calib[24] )  # 湿度補正データ(BME280では使うがBMP280では不使用)
-            self.digH[1] = ((calib[26]<< 8) | calib[25])
-            self.digH[2] = ( calib[27] )
-            self.digH[3] = ((calib[28]<< 4) | (0x0F & calib[29]))
-            self.digH[4] = ((calib[30]<< 4) | ((calib[29] >> 4) & 0x0F))
-            self.digH[5] = ( calib[31] )
+            self._digT[0] = ((calib[1] << 8) | calib[0])  # 気温補正データ
+            self._digT[1] = ((calib[3] << 8) | calib[2])
+            self._digT[2] = ((calib[5] << 8) | calib[4])
+            self._digP[0] = ((calib[7] << 8) | calib[6])  # 気圧補正データ
+            self._digP[1] = ((calib[9] << 8) | calib[8])
+            self._digP[2] = ((calib[11]<< 8) | calib[10])
+            self._digP[3] = ((calib[13]<< 8) | calib[12])
+            self._digP[4] = ((calib[15]<< 8) | calib[14])
+            self._digP[5] = ((calib[17]<< 8) | calib[16])
+            self._digP[6] = ((calib[19]<< 8) | calib[18])
+            self._digP[7] = ((calib[21]<< 8) | calib[20])
+            self._digP[8] = ((calib[23]<< 8) | calib[22])
+            self._digH[0] = ( calib[24] )  # 湿度補正データ(BME280では使うがBMP280では不使用)
+            self._digH[1] = ((calib[26]<< 8) | calib[25])
+            self._digH[2] = ( calib[27] )
+            self._digH[3] = ((calib[28]<< 4) | (0x0F & calib[29]))
+            self._digH[4] = ((calib[30]<< 4) | ((calib[29] >> 4) & 0x0F))
+            self._digH[5] = ( calib[31] )
             
             # 補正用パラメータを適切な形式に変換
             ## この部分を削除すると気圧が1090hPaくらいの異常値になります
             for i in range(1,2):
-                if self.digT[i] & 0x8000:
-                    self.digT[i] = (-self.digT[i] ^ 0xFFFF) + 1
+                if self._digT[i] & 0x8000:
+                    self._digT[i] = (-self._digT[i] ^ 0xFFFF) + 1
 
             for i in range(1,8):
-                if self.digP[i] & 0x8000:
-                    self.digP[i] = (-self.digP[i] ^ 0xFFFF) + 1
+                if self._digP[i] & 0x8000:
+                    self._digP[i] = (-self._digP[i] ^ 0xFFFF) + 1
 
             for i in range(0,6):
-                if self.digH[i] & 0x8000:
-                    self.digH[i] = (-self.digH[i] ^ 0xFFFF) + 1  
+                if self._digH[i] & 0x8000:
+                    self._digH[i] = (-self._digH[i] ^ 0xFFFF) + 1  
         except Exception as e:
             self.logger.exception("An error occured!")
 
@@ -182,10 +182,10 @@ class BMP280:
     # 気温の生データを℃単位に補正
     def _compensate_T(self, adc_T):
         try:
-            v1 = (adc_T / 16384.0 - self.digT[0] / 1024.0) * self.digT[1]
-            v2 = (adc_T / 131072.0 - self.digT[0] / 8192.0) * (adc_T / 131072.0 - self.digT[0] / 8192.0) * self.digT[2]
-            self.t_fine = v1 + v2
-            temperature = self.t_fine / 5120.0
+            v1 = (adc_T / 16384.0 - self._digT[0] / 1024.0) * self._digT[1]
+            v2 = (adc_T / 131072.0 - self._digT[0] / 8192.0) * (adc_T / 131072.0 - self._digT[0] / 8192.0) * self._digT[2]
+            self._t_fine = v1 + v2
+            temperature = self._t_fine / 5120.0
 
             return temperature
         except Exception as e:
@@ -196,12 +196,12 @@ class BMP280:
         try:
             pressure = 0.0
             
-            v1 = (self.t_fine / 2.0) - 64000.0
-            v2 = (((v1 / 4.0) * (v1 / 4.0)) / 2048) * self.digP[5]
-            v2 = v2 + ((v1 * self.digP[4]) * 2.0)
-            v2 = (v2 / 4.0) + (self.digP[3] * 65536.0)
-            v1 = (((self.digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8)  + ((self.digP[1] * v1) / 2.0)) / 262144
-            v1 = ((32768 + v1) * self.digP[0]) / 32768
+            v1 = (self._t_fine / 2.0) - 64000.0
+            v2 = (((v1 / 4.0) * (v1 / 4.0)) / 2048) * self._digP[5]
+            v2 = v2 + ((v1 * self._digP[4]) * 2.0)
+            v2 = (v2 / 4.0) + (self._digP[3] * 65536.0)
+            v1 = (((self._digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8)  + ((self._digP[1] * v1) / 2.0)) / 262144
+            v1 = ((32768 + v1) * self._digP[0]) / 32768
             
             if v1 == 0:
                 return 0
@@ -210,9 +210,9 @@ class BMP280:
                 pressure = (pressure * 2.0) / v1
             else:
                 pressure = (pressure / v1) * 2
-            v1 = (self.digP[8] * (((pressure / 8.0) * (pressure / 8.0)) / 8192.0)) / 4096
-            v2 = ((pressure / 4.0) * self.digP[7]) / 8192.0
-            pressure = pressure + ((v1 + v2 + self.digP[6]) / 16.0)  
+            v1 = (self._digP[8] * (((pressure / 8.0) * (pressure / 8.0)) / 8192.0)) / 4096
+            v2 = ((pressure / 4.0) * self._digP[7]) / 8192.0
+            pressure = pressure + ((v1 + v2 + self._digP[6]) / 16.0)  
 
             return pressure / 100
         except Exception as e:
@@ -220,12 +220,12 @@ class BMP280:
     
     # 湿度の生データを%単位に補正
     def _compensate_H(self, adc_H):
-        v1 = self.t_fine - 76800.0
+        v1 = self._t_fine - 76800.0
         if v1 != 0:
-            v1 = (adc_H - (self.digH[3] * 64.0 + (self.digH[4] / 16384.0) * v1)) * (
-                self.digH[1] / 65536.0 * (1.0 + (self.digH[5] / 67108864.0) * v1 * (1.0 + (self.digH[2] / 67108864.0) * v1)))
+            v1 = (adc_H - (self._digH[3] * 64.0 + (self._digH[4] / 16384.0) * v1)) * (
+                self._digH[1] / 65536.0 * (1.0 + (self._digH[5] / 67108864.0) * v1 * (1.0 + (self._digH[2] / 67108864.0) * v1)))
 
-        humidity = v1 * (1.0 - self.digH[0] * v1 / 524288.0)
+        humidity = v1 * (1.0 - self._digH[0] * v1 / 524288.0)
         if humidity > 100.0:
             humidity = 100.0
         elif humidity < 0.0:
