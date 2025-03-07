5d437c2b4d43532eda82e234745c02c411214721
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-03-07 18:40:22 +0900

    SG90の回転角度と補正
    SG90の回転角度次第で一体いくつframe_center_xの補正をかければ？

--- testcode/sg90_camera.py
+++ testcode/sg90_camera.py
@@ -5,6 +5,79 @@ import numpy as np
 from picamera2 import Picamera2
 
 
+class SG90:
+    """SG90を制御するクラス
+
+    pin: GPIOのピン番号
+    max_angle: 最大移動角度
+    min_angle: 最小移動角度
+    angle: 現在の角度
+    ini_angle: 初期設定角度
+    pi: gpio制御
+    """
+    def __init__(self, pin, min_angle=0, max_angle=180, ini_angle=0, freq=50, logger=None):
+        """サーボモータをセットアップ"""
+
+        # もしloggerが渡されなかったら，ログの記録先を標準出力にする
+        if logger is None:
+            logger = getLogger(__name__)
+            logger.addHandler(StreamHandler())
+            logger.setLevel(10)
+        self._logger = logger
+
+        self._pin = pin
+        self._max_angle = max_angle
+        self._min_angle = min_angle
+        self._angle = 0
+        self._ini_angle = ini_angle
+        self._freq = freq
+        self._range = 255
+
+        self._pi = pigpio.pi()
+        self._pi.set_mode(self._pin, pigpio.OUTPUT)
+
+        self.set_ini_angle()
+    
+    def __del__(self):
+        """サーボモータを終了"""
+        if self._pi.connected:
+            self._pi.set_mode(self._pin, pigpio.INPUT)
+            # self._pi.stop()
+
+    def get_angle(self):
+        """サーボモータの角度を取得"""
+        return self._angle
+
+    #set_servo_pulsewidthを使わない方法(案)
+    def set_angle(self, target_angle):
+        """サーボモータの角度を特定の角度に動かす"""
+        if target_angle < self._min_angle or target_angle > self._max_angle:
+            self._logger.warning(f"角度は{self._min_angle}から{self._max_angle}度の間で指定してください。")
+            return
+        
+        #角度[degree]→パルス幅[μs]に変換
+        pulse_width = ((target_angle)/180.0) * 1900.0+500.0
+        self._angle = target_angle
+        #duty比[%]を計算(周期：20ms=20000μs)
+        pwm_duty = 100.0 * (pulse_width/20000.0)
+        #duty比をhardware_PWMに使える形に変換(1,000,000を100%と考えて数値を指定するらしい.整数で表したいとかなんとか.)
+        duty_cycle = int(pwm_duty * 1000000 / 100)
+        frequency = int(self._freq)
+        
+        #PWM出力
+        # hardware-PWM バージョン
+        # self.pi.hardware_PWM(self.pin,frequency,duty_cycle)
+        # software-PWM バージョン
+        self._pi.set_PWM_frequency(self._pin, frequency)
+        self._pi.set_PWM_range(self._pin, self._range)
+        self._pi.set_PWM_dutycycle(self._pin, int((pwm_duty / 100) * self._range))
+
+        self._logger.info(f"servo_angle: {self._angle}")
+    
+    def set_ini_angle(self):
+        self.angle = self._ini_angle
+
+
 class Camera:
     def red_detect(self, frame):
         # HSV色空間に変換
@@ -90,6 +163,8 @@ class Camera:
         return frame, camera_order
 
 if __name__ == '__main__':
+    sg90 = SG90(pin=26, min_angle=-90, max_angle=90, ini_angle=0, freq=50)
+
     picam2 = Picamera2()
     config = picam2.create_preview_configuration({"format": 'XRGB8888', "size": (320, 240)})
     picam2.configure(config)
@@ -98,6 +173,39 @@ if __name__ == '__main__':
 
     while True:
         frame = picam2.capture_array()
+        mask = cam.red_detect(frame)
+        # 赤色検知の結果を取得
+        # analize_redの戻り値は0が見つからない，1が中心，2が右，3が左，4がゴール
+        frame, camera_order = cam.analyze_red(frame, mask)
+        # 結果表示
+        cv2.imshow('kekka', frame)
+        time.sleep(3)
+        #print(len(contours))
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            print('q interrupted direction by camera')
+            continue
+            
+        sg90.set_angle(-15)
+        frame = picam2.capture_array()
+        mask = cam.red_detect(frame)
+        # 赤色検知の結果を取得
+        # analize_redの戻り値は0が見つからない，1が中心，2が右，3が左，4がゴール
+        frame, camera_order = cam.analyze_red(frame, mask)
+        # 結果表示
+        cv2.imshow('kekka', frame)
+        time.sleep(3)
+        #print(len(contours))
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            print('q interrupted direction by camera')
+            continue
+
+
+        sg90.set_angle(15)
+        frame = picam2.capture_array()
         cam.red_detect(frame)
         mask = cam.red_detect(frame)
         # 赤色検知の結果を取得
@@ -105,10 +213,11 @@ if __name__ == '__main__':
         frame, camera_order = cam.analyze_red(frame, mask)
         # 結果表示
         cv2.imshow('kekka', frame)
-        time.sleep(1)
+        time.sleep(3)
         #print(len(contours))
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             print('q interrupted direction by camera')
             continue
+
