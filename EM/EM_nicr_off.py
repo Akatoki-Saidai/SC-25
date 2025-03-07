bb87c348357ac674bf6fa1dfdfdbf55b5139a943
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-03-08 00:58:13 +0900

    テスト用

--- EM/EM_nicr_off.py
+++ EM/EM_nicr_off.py
@@ -93,7 +93,7 @@ def wait_phase(devices, data):
             # 高度が十分高かったら
             if 0 < data["alt"]:
                 old_alt = data["alt"]
-                time.sleep(3)
+                time.sleep(1)
                 # 少し待ってもまだ高度が高く，かつ高度が少しでも変化していたら，待機フェーズを終了
                 if 2 < data["alt"] and old_alt != data["alt"]:
                     break
